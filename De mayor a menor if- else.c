320f2299cc9fc3563ab3e2f968e148576eb9a962
MicaelaCruz13 <micacruz1304@gmail.com>
2025-04-25 11:49:48 -0300

    Add files via upload

---
+++ De mayor a menor if- else.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int num1, num2, num3;
+	
+	printf("Ingrese el primer número: ");
+	scanf("%d", &num1);
+	
+	printf("Ingrese el segundo número: ");
+	scanf("%d", &num2);
+	
+	printf("Ingrese el tercer numero: ");
+	scanf("%d", &num3);
+	
+	if (num1<num2){ 
+      (num1<num2);
+		
+	} 
+	else{  
+		 (num2>num1);
+	}
+	if (num1<num3){ 
+		(num1<num3);
+		
+	} 
+	else{  
+		(num3>num2);
+	}
+	if (num2<num3){ 
+		(num2<num3);
+		
+	} 
+	else{  
+		(num3>num2);
+	}
+	
+		
+	printf("El orden de mayor a menor de los 3 numeros es: %d>%d>%d ", num1, num2, num3);
+	
+
+	return 0;
+}
---
+++ primer trabajo bien hecho.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int num1, num2, num3, X;
+	
+	printf("Ingrese el primer número: ");
+	scanf("%d", &num1);
+	
+	printf("Ingrese el segundo número: ");
+	scanf("%d", &num2);
+	
+	printf("Ingrese el tercer numero: ");
+	scanf("%d", &num3);
+	
+	if (num1<num2){ 
+		X=num1;
+		num1=num2;
+		num2 = X; 
+		
+	}
+	if (num1<num3){ 
+		X=num1;
+		num1=num3;
+		num3 = X; 
+		
+	}
+	if (num2<num3){ 
+		X=num2;
+		num2=num3;
+		num3 = X; 
+		
+	}
+	printf("El orden de mayor a menor de los 3 numeros es: %d>%d>%d ", num1, num2, num3);
+	
+	
+	return 0;
+}
+
