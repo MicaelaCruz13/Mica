e4c220ea22b05b7c5f29cd2f1a82172c505e479d
MicaelaCruz13 <micacruz1304@gmail.com>
2025-04-25 13:15:19 -0300

    Add files via upload

---
+++ datos del usuario.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	int nombre;
+	int edad;
+	float altura;
+
+		printf ("Hola Mundo\n");
+		printf ("Introducir nombre: " );
+		scanf ("%s", &nombre);
+		
+		printf ("Introducir edad:\n");
+		scanf ("%d", &edad);
+		
+		printf ("Introducir altura:\n");
+		scanf ("%f", &altura);
+	
+		
+	return 0;
+}
+
