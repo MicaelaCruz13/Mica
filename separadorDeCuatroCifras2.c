037b7af7917797ba71c7272c7ed27d40380683d2
MicaelaCruz13 <micacruz1304@gmail.com>
2025-04-25 13:05:59 -0300

    Add files via upload

---
+++ separadorDeCuatroCifras2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	int numero, uni,dec,cent,uDeMil;
+	
+	printf("Ingrese un número de cuatro cifras: ");
+	scanf("%d", &numero);
+	
+	if (numero < 1000 || numero > 9999) {
+		printf("------------------------------------\n");
+		printf("   Ingrese un Numero de CUATRO cifras!\n");
+		printf("\tPrograma Finalizado...\n");
+		printf("------------------------------------\n");
+		return 1;
+	}
+	
+	uDeMil = numero / 1000;
+	cent = (numero/100) % 10;
+	dec = (numero/10) % 10;
+	uni = numero % 10;
+	
+	printf("------------------------------------\n");
+	printf("%d --> %d|%d|%d|%d\n",numero,uDeMil,cent,dec,uni);
+	printf("------------------------------------\n");;
+	
+	return 0;
+}
